3b9c2b45622090d8e5ebb2d7df7619b40a3ab1eb
王贤 <Yan@Yan-MacBook-Pro.lan>
2015-12-02 17:58:21 +0800

    cao...

---
+++ web/config/env/development.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	log: {
+		// Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+		format: 'dev',
+		// Stream defaults to process.stdout
+		// Uncomment to enable logging to a log on the file system
+		options: {
+			//stream: 'access.log'
+		}
+	},
+	app: {
+		title: 'MEAN.JS - Development Environment'
+	}
+};
--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,5 +1,35 @@
 var gulp = require('gulp');
+//var connect = require('gulp-connect');
+var server = require('gulp-express');
+/*
+gulp.task('webserver', function () {
+    connect.server({
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
+    });
+
 });
+*/
+
+gulp.task('server', function () {
+    // Start the server at the beginning of the task
+    server.run(['server.js']);
+
+    // Restart the server when file changes
+    gulp.watch(['app/**/*.html'], server.notify);
+    gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
+    //gulp.watch(['{.tmp,app}/styles/**/*.css'], ['styles:css', server.notify]);
+    //Event object won't pass down to gulp.watch's callback if there's more than one of them.
+    //So the correct way to use server.notify is as following:
+    gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
+        gulp.run('styles:css');
+        server.notify(event);
+        //pipe support is added for server.notify since v0.1.5,
+        //see https://github.com/gimm/gulp-express#servernotifyevent
+    });
+
+    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+    gulp.watch(['app/images/**/*'], server.notify);
+    gulp.watch(['server.js', 'routes/**/*.js'], [server.run]);
+});
+
+gulp.task('default', ['server']);
