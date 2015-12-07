5ae641136275ba8b5aca073914e69369d642e034
Yan <178312136@qq.com>
2015-12-07 14:58:52 +0800

    update  fucking 'menu height' bug

--- web/public/modules/core/directives/home.admin.directive.js
+++ web/public/modules/core/directives/home.admin.directive.js
@@ -2,20 +2,19 @@
  * Created by Yan on 15/12/3.
  */
 "use strict";
-//var geekDirectives = angular.module('ceDirectives', []);
 
-
-angular.module('core').directive('ceMenu', [
-    function() {
+angular.module('core').directive('ceMenu', ['$window','$document',
+    function($window,$document) {
         var menu={
             restrict:'EA',
             templateUrl:'modules/core/views/templates/menu.admin.template.html',
             replace:true,
-            link: function(scope, ele, attrs) {
-                //$(ele).css('height',($(window).height()+$(window).scrollTop())-45);
-                //ele.css('height',($(window).height()+$(window).scrollTop())-45);
-                //ele.css('height',((window).height()+(window).scrollTop())-45);
-
+            link: function(scope, ele) {
+                //不应该写死高度
+                //todo 继续研究怎么获取head的高度
+                var headHeight = 45;
+                var height = $window.innerHeight - headHeight;
+                ele.css({height:height+"px"});
             }
         };
         return menu;
