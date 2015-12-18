8c7d969c181cad15ccfe99bc58f6fea5acb85393
Yan <178312136@qq.com>
2015-12-18 16:45:09 +0800

    完美得一塌糊涂的代码

--- web/public/modules/core/directives/home.admin.directive.js
+++ web/public/modules/core/directives/home.admin.directive.js
@@ -4,7 +4,7 @@
 "use strict";
 
 angular.module('core').directive('ceMenu', ['$window','$document',
-    function($window) {
+    function($window,$document) {
         var menu={
             restrict:'EA',
             templateUrl:'modules/core/views/templates/menu.admin.template.html',
@@ -13,16 +13,27 @@ angular.module('core').directive('ceMenu', ['$window','$document',
 
                 //todo 继续研究怎么获取head的高度,不应该写死高度
                 var headHeight = 45;
+
                 scope.onResize = function() {
+                    var contentHeight = angular.element(document.querySelector('.ce-content')).height();
                     var height = $window.innerHeight - headHeight;
-                    ele.css({height:height+"px"});
+                    if(contentHeight > height){
+                        ele.css({height:contentHeight+"px"});
+                    }else{
+                        ele.css({height:height+"px"});
+                    }
+
                 };
                 scope.onResize();
                 angular.element($window).bind('resize', function() {
                     scope.onResize();
-
+                });
+                angular.element($window).bind('scroll', function() {
+                    scope.onResize();
                 });
             }
         };
+
+
         return menu;
     }]);
