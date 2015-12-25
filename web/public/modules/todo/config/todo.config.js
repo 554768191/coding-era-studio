24742a29991f178cf31db3119120b2b43b8f1d38
Yan <178312136@qq.com>
2015-12-25 14:40:55 +0800

    重新命名

--- web/public/modules/todo/config/todo.config.js
+++ web/public/modules/todo/config/todo.config.js
@@ -2,7 +2,22 @@
  * Created by Yan on 15/12/3.
  */
 'use strict';
-angular.module('todo').config(['$stateProvider','$translatePartialLoaderProvider',
+angular.module('todo').run(['Menus',
+    function (Menus) {
+        // Menus.addMenus();
+        var parentMenus = Menus.genParentMenus({name:'JASON的秘密基地', icon:'check'});
+        var jason1 = Menus.genNodeMenus({name:'TODO', icon:'tasks', route:'todo'});
+        var jason2 = Menus.genNodeMenus({name:'GitHub Search', icon:'search', route:'search'});
+        var jason3 = Menus.genNodeMenus({name:'计时器', icon:'time', route:'timer'});
+        var jason4 = Menus.genNodeMenus({name:'精要指令', icon:'king', route:'directiveKing'});
+        parentMenus.setOrder(99);
+        parentMenus.addNodeMenus(jason1);
+        parentMenus.addNodeMenus(jason2);
+        parentMenus.addNodeMenus(jason3);
+        parentMenus.addNodeMenus(jason4);
+        Menus.addMenus(parentMenus.getMenus());
+    }
+]).config(['$stateProvider','$translatePartialLoaderProvider',
     function ($stateProvider,$translatePartialLoaderProvider) {
         //国际化
         $translatePartialLoaderProvider.addPart('todo');
