48838d020ca742346501150437721eab18f76b57
Yan <178312136@qq.com>
2016-03-14 15:37:53 +0800

    删掉再改

--- CodingEraAdmin/public/modules/demo/demo.module.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Yan on 15/12/3.
- */
-'use strict';
-
-// Use Application configuration module to register a new module
-ApplicationConfiguration.registerModule('demo');
--- CodingEraAdmin/public/modules/dynamic/dynamic.module.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Jason.
- */
-'use strict';
-
-// Use Application configuration module to register a new module
-ApplicationConfiguration.registerModule('dynamic');
