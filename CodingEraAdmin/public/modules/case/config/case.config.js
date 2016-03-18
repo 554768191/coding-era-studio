b5d0009b2d96651155c3de8e42de35bdca05c278
Yan <178312136@qq.com>
2016-03-18 08:30:33 +0800

    改烂我代码

--- CodingEraAdmin/public/modules/case/config/case.config.js
+++ CodingEraAdmin/public/modules/case/config/case.config.js
@@ -7,17 +7,15 @@ angular.module('case')
     .run(['Menus',
     function(Menus) {
 
-        var caseMenu = Menus.genParentMenus({name: '作品管理', icon: 'th-large'});
+        var caseMenu = Menus.genParentMenus({name: '作品', icon: 'th-large'});
         caseMenu.setOrder(-1);
 
         //CASE
-        var node_case = Menus.genNodeMenus({name: '发布作品', subTitle: '发布门户网站的作品', icon: 'plus', route: 'case/publish'});
-        var node_case_list = Menus.genNodeMenus({name: '作品列表', subTitle: '管理作品列表', icon: 'search', route: 'case/list'});
-        caseMenu.addNodeMenus(node_case);
+        var node_case_list = Menus.genNodeMenus({name: '作品列表', subTitle: '管理作品列表', icon: 'th-list', route: 'caseManage'});
         caseMenu.addNodeMenus(node_case_list);
 
         //TAG
-        var node_tag_list = Menus.genNodeMenus({name: '标签管理', subTitle: '管理标签列表', icon: 'tag', route: 'tag/list'});
+        var node_tag_list = Menus.genNodeMenus({name: '标签管理', subTitle: '管理标签列表', icon: 'tag', route: 'tagList'});
         caseMenu.addNodeMenus(node_tag_list);
 
         Menus.addMenus(caseMenu.getMenus());
@@ -28,23 +26,28 @@ angular.module('case')
         //国际化
         $translatePartialLoaderProvider.addPart('case');
         $stateProvider
-            .state('case/publish', {
-                url: '/case/publish',
-                templateUrl: 'modules/case/views/case.publish.view.html',
-                controller:'casePublishCtrl'
+            .state('caseManage', {
+                url: '/case',
+                templateUrl: 'modules/case/views/case.manage.view.html',
+                controller:'caseManageCtrl'
             })
-            .state('case/list', {
-                url: '/case/list',
+            .state('caseManage.list', {
+                url: '/list',
                 templateUrl: 'modules/case/views/case.list.view.html',
                 controller:'caseListCtrl'
             })
-            .state('case/edit', {
-                url: '/case/publish',
+            .state('caseManage.publish', {
+                url: '/publish',
+                templateUrl: 'modules/case/views/case.publish.view.html',
+                controller:'casePublishCtrl'
+            })
+            .state('tagEdit', {
+                url: '/case/tag/edit',
                 templateUrl: 'modules/case/views/case.tag.edit.view.html',
                 controller:'tagEditCtrl'
             })
-            .state('tag/list', {
-                url: '/tag/list',
+            .state('tagList', {
+                url: '/case/tag/list',
                 templateUrl: 'modules/case/views/case.tag.list.view.html',
                 controller:'tagListCtrl'
             });
