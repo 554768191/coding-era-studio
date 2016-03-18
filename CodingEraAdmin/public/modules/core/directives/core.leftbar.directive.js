a4c7e11da304c7a6a0b4de3f5cec55cf0633f60e
Yan <178312136@qq.com>
2016-03-18 23:00:12 +0800

    再次解决leftbar高度问题

--- CodingEraAdmin/public/modules/core/directives/core.leftbar.directive.js
+++ CodingEraAdmin/public/modules/core/directives/core.leftbar.directive.js
@@ -8,12 +8,23 @@ angular.module('core').directive('ceLeftbar', ['$window','$rootScope',
         var leftbar={
             restrict:'C',
             link: function(scope, ele) {
-                var contentView = angular.element(document.querySelector('.ce-content-view'));
-                $rootScope.contentView = contentView[0];
-                $rootScope.$watch('contentView.scrollHeight',function(abc,efg){
-                    ele.css({height:abc + 'px'});
+                //监控leftbar高度变化
+                $rootScope.$watch(function(){
+                    var contentView = angular.element(document.querySelector('.ce-rightContent'));
+                    return contentView.height();
+                },function(newHeight){
+                    //15 = leftbar.marginTop = -15
+                    //20 = leftbar.paddingTop = 20;
+                    var contentViewHeight = newHeight + 15 + 20;//右边内容高度
+                    var headHeight = angular.element(document.querySelector('.ce-head')).height();
+                    var windowHeight = $window.innerHeight;
+                    var innerHeight =  windowHeight - headHeight;//主窗体高度 - 头部高度
+                    if(contentViewHeight > innerHeight){
+                        ele.css({height:contentViewHeight + 'px'});
+                    }else{
+                        ele.css({height:innerHeight + 'px'});
+                    }
                 });
-                
             }
         };
 
