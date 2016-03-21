e817d9118bd96e3cb8da62b5ad3ef89296b13feb
Yan <178312136@qq.com>
2016-03-22 00:02:47 +0800

    CodingEraAdmin -解决Loading通知问题

--- CodingEraAdmin/public/modules/core/directives/core.loadingbar.directive.js
+++ CodingEraAdmin/public/modules/core/directives/core.loadingbar.directive.js
@@ -4,19 +4,25 @@
 "use strict";
 
 angular.module('core')
-    .directive('ceLoadingBar', ['$rootScope', function($rootScope) {
+    .directive('ceLoadingBar', ['$rootScope','$timeout', function($rootScope,$timeout) {
         return {
             restrict: 'EA',
             templateUrl:'modules/core/views/templates/core.loadingbar.template.html',
+            scope:true,
             link: function(scope, el, attrs) {
                 $rootScope.$on('startLoading', function() {
-                    scope.show=true;
-                    scope.$apply();
+                    scope.show = true;
+                    $timeout(function(){
+                        scope.$apply();
+                    });
+
                 });
 
                 $rootScope.$on('stopLoading', function() {
-                    scope.show=false;
-                    scope.$apply();
+                    scope.show = false;
+                    $timeout(function(){
+                        scope.$apply();
+                    });
                 });
             }
         };
