3d9bfad79792e0aacdd745aa0e53b770c657b268
JasonWoo <794054202@qq.com>
2016-05-25 12:05:24 +0800

    CodingEraPortal:fixbug链接超时服务立马挂掉

--- CodingEraPortal/public/modules/common/server/controllers/request.server.controller.js
+++ CodingEraPortal/public/modules/common/server/controllers/request.server.controller.js
@@ -14,22 +14,16 @@ var config = require(path.resolve('./config/config'));
 
 exports.get = function (url, done) {
     function callback(error, response, body) {
-        //我们后台不标准,暂时修改
-        //if (!error && response.statusCode == 200) {
-        //    done(null, JSON.parse(body));
-        //} else {
-        //    done(JSON.parse(body));
-        //}
         //我们后台不标准,暂时修改(针对Service做匹配)
-        if (!error && response.result == 'success') {
+        console.log("jasn2...................:",response.statusCode);
+        if (!error && response.statusCode == 200) {
             done(JSON.parse(body));
         } else {
-            if(body){
-                done(JSON.parse(body));
-            }else{
+            if(response.statusCode == 404){
                 done({result:'error',message:' o(╯□╰)o 服务器失联了,请稍后再试'});
+            }else{
+                done({result:'error',message:' o(╯□╰)o 请求出错了,我们会尽快处理'});
             }
-
         }
     }
     request(config.apiURL + url, callback);
