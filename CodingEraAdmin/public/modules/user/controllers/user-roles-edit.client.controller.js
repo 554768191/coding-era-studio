d1ad2d2867bc479e89a858a7af9d7fb3830ad779
JasonWoo <794054202@qq.com>
2016-06-05 22:46:10 +0800

    ceAdmin:1.用户角色编辑

--- CodingEraAdmin/public/modules/user/controllers/user-roles-edit.client.controller.js
+++ CodingEraAdmin/public/modules/user/controllers/user-roles-edit.client.controller.js
@@ -19,18 +19,25 @@ angular.module('user').controller('userRolesEditCtrl', [
             that.roles = data.roles;
             that.item = data.user;
 
-            //视图中使用了数组/MAP,如果立刻绑定数据,会显示错误
-            $timeout(function() {
-                UserService.loadAuthoritiesToView( that.item );
+            UserService.findUserRolesByUserId({userId:that.item.id}).success(function (res) {
+                //视图中使用了数组/MAP,如果立刻绑定数据,会显示错误
+                //$timeout(function() {
+                //},500);
+                //UserService.loadAuthoritiesToView( that.item );
+                UserService.loadUserRolesToView( that.item, res.data );
                 //传递最底层checkbox勾选状态到其父级checkbox
                 that.$broadcast('checkboxChangedToParents', 'role');
-            },500);
+            });
         };
         that.init();
 
         //保存
         that.onSaveClick = function () {
-            RoleService.saveRolePermissions(RoleService.loadViewToRolePermissions(that.item)).success(function (res) {
+            UserService.saveUserRoles({
+                userRoles:UserService.loadViewToUserRoles(that.item),
+                user:null,
+                userId:that.item.id
+            }).success(function (res) {
                 ceUtil.toast('保存成功');
                 $uibModalInstance.close(res.data);
             });
--- CodingEraAdmin/public/modules/user/services/user.client.service.js
+++ CodingEraAdmin/public/modules/user/services/user.client.service.js
@@ -18,16 +18,58 @@ angular.module('user').factory('UserService', ['$log','$http', 'ceAjax',
             return ceAjax.post({url: '/user', data: parameters});
         };
 
-        // 把API返回的数据转成View可以显示的格式
+        service.saveUserRoles = function (userRolesView) {
+            return ceAjax.post({url: '/user/roles', data: userRolesView});
+        };
+
+        service.findUserRolesByUserId = function (parameters) {
+            return ceAjax.get({url: '/user/roles', data: parameters});
+        };
+
+        // 把API返回的数据转成View可以显示的格式 (备用)
         // "authorities":[{"authority":"ROLE_ADMIN"},{"authority":"ROLE_USER"},{"authority":"ROLE_MOBILE"}]
         service.loadAuthoritiesToView = function(user){
             angular.forEach(user.authorities, function (authority, key){
-                if(angular.isUndefined(user.roles[authority.authority])){
+                if(angular.isUndefined(user.userRoles[authority.authority])){
                     return;
                 }
-                user.roles[authority.authority].checked = true;
+                user.userRoles[authority.authority].checked = true;
             });
         };
+
+        // 把API返回的数据转成View可以显示的格式
+        // "roles":[{"id":5,"createdTime":null,"version":0,"role":"ROLE_USER"},{"id":6,"createdTime":null,"version":0,"role":"ROLE_JASON"}]
+        service.loadUserRolesToView = function(user, userRoles){
+            angular.forEach(userRoles, function (userRole, key){
+                if(angular.isUndefined(user.userRoles[userRole.role])){
+                    return;
+                }
+                angular.extend(user.userRoles[userRole.role], userRole);
+                user.userRoles[userRole.role].checked = true;
+            });
+        };
+
+        service.loadViewToUserRoles = function(user){
+            var saveList = [];
+            angular.forEach(user.userRoles, function (userRole, key) {
+                if(userRole.id){
+                    if(userRole.checked !== true){
+                        userRole.role = null;
+                    }
+                    userRole.userId = user.id;
+                    saveList.push(userRole);
+                }else{
+                    if(userRole.checked === true) {
+                        userRole.userId = user.id;
+                        userRole.role = key;
+                        saveList.push(userRole);
+                    }
+                }
+            });
+
+            return saveList;
+        };
+
 
         return service;
     }
