4ce9ef0f1aa35e9629aaa4f5cf2cb1e4ef3b2631
JasonWoo <794054202@qq.com>
2016-06-12 16:40:35 +0800

    ceAdmin:1.user模块完美添加权限

--- CodingEraAdmin/public/modules/user/config/user.client.config.js
+++ CodingEraAdmin/public/modules/user/config/user.client.config.js
@@ -4,7 +4,9 @@
 angular.module('user').run([
     'Menus',
     function (Menus) {
-        var userMenu = Menus.genMenu({name:'管理', subTitle:'管理后台设置', icon:'cog', route: 'usersManage'});
+        var userMenu = Menus.genMenu({
+            name:'管理', subTitle:'管理后台设置', icon:'cog', route: 'usersManage'
+        });
         userMenu.setOrder(99);
         Menus.addMenus(userMenu.getMenus());
     }
@@ -17,24 +19,24 @@ angular.module('user').run([
             .state('usersManage', {
                 url: '/user',
                 templateUrl: 'modules/user/views/user-manage.client.view.html',
-                controller: 'usersManageCtrl',
-                secured:"hasPermission('article','read')"
+                controller: 'usersManageCtrl'
             })
             .state('usersManage.list', {
                 url: '/list?:status',
                 templateUrl: 'modules/user/views/user-list.client.view.html',
+                secured:"hasPermission('user','read')"
             })
             .state('usersManage.edit', {
                 url: '/edit?:username',
                 templateUrl: 'modules/user/views/user-profile.client.view.html',
-                controller:'UserProfileController'
+                controller:'UserProfileController',
+                secured:"hasPermission('user','write')"
             })
             // 个人信息
             .state('usersManage.profile', {
                 url: '/profile',
                 templateUrl: 'modules/user/views/user-profile.client.view.html',
-                controller:'UserProfileController',
-                secured:"hasPermission('article','read')"
+                controller:'UserProfileController'
             })
             // 个人信息 -> 修改头像
             .state('usersManage.avatar', {
@@ -46,53 +48,59 @@ angular.module('user').run([
             .state('usersManage.roles', {
                 url: '/roles/edit?:userId',
                 templateUrl: 'modules/user/views/user-roles-edit.client.view.html',
-                controller:'userRolesEditCtrl'
+                controller:'userRolesEditCtrl',
+                secured:"hasPermission('user','write')"
             })
 
             // 角色管理
             .state('usersManage.rolesManage', {
                 url: '/role',
                 templateUrl: 'modules/user/views/permission/user-role-list.client.view.html',
-                secured:"hasPermission('article','read1')"
+                secured:"hasPermission('user','read')"
             })
             .state('usersManage.rolesManage.edit', {
                 url: '/edit?:roleId',
                 templateUrl: 'modules/user/views/permission/user-role-edit.client.view.html',
-                controller:'userRoleEditCtrl'
+                controller:'userRoleEditCtrl',
+                secured:"hasPermission('user','write')"
             })
             .state('usersManage.rolesManage.permissions', {
                 url: '/permissions/edit?:roleId',
                 templateUrl: 'modules/user/views/permission/user-role-permissions-edit.client.view.html',
-                controller:'userRolePermissionsEditCtrl'
+                controller:'userRolePermissionsEditCtrl',
+                secured:"hasPermission('user','write')"
             })
             // 资源管理
             .state('usersManage.resourcesManage', {
                 url: '/resources',
-                templateUrl: 'modules/user/views/permission/user-resource-list.client.view.html'
+                templateUrl: 'modules/user/views/permission/user-resource-list.client.view.html',
+                secured:"hasPermission('user','read')"
             })
             .state('usersManage.resourcesManage.edit', {
                 url: '/edit?:resourceId',
                 templateUrl: 'modules/user/views/permission/user-resource-edit.client.view.html',
-                controller:'userResourceEditCtrl'
+                controller:'userResourceEditCtrl',
+                secured:"hasPermission('user','write')"
             })
             // 权限管理
             .state('usersManage.permissionsManage', {
                 url: '/permissions',
                 templateUrl: 'modules/user/views/permission/user-permission-list.client.view.html',
-                secured:"hasPermission('article','read')"
+                secured:"hasPermission('user','read')"
             })
             .state('usersManage.permissionsManage.edit', {
                 url: '/edit?:permissionId',
                 templateUrl: 'modules/user/views/permission/user-permission-edit.client.view.html',
                 controller:'userPermissionEditCtrl',
-                secured:"hasPermission('article','read1')"
+                secured:"hasPermission('user','write')"
             })
 
             // 重置密码
             .state('usersManage.resetPassword', {
                 url: '/password/reset/:token',
                 templateUrl: '/modules/user/views/user-reset-password.client.view.html',
-                controller:'PasswordController'
+                controller:'PasswordController',
+                secured:"hasPermission('user','write')"
             })
 
             //公众页面 重置密码
