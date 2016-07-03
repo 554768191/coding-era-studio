8c5d3e3df2ab9a461a06f7a6254cd27be70eda36
JasonWoo <794054202@qq.com>
2016-07-03 21:40:53 +0800

    CodingEraPortal+ceAdmin:生产启动命令统一为npm start

---
+++ CodingEraPortal/index.js
@@ -0,0 +1,26 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+var allAssets = require('./config/allAssets'),
+	chalk = require('chalk');
+
+
+// Init the express application
+var app = require('./config/express')();
+
+// Start the app by listening on <port>
+app.listen(allAssets.port);
+
+// Expose app
+//exports = module.exports = app;
+
+// Logging initialization
+console.log('--');
+console.log(chalk.green(allAssets.app.title + ' application started'));
+console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
+console.log(chalk.green('Port:\t\t\t\t' + allAssets.port));
+if (process.env.NODE_ENV === 'secure') {
+	console.log(chalk.green('HTTPs:\t\t\t\ton'));
+}
+console.log('--');
